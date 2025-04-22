1320b36246c18d34bd218395180b288877c04f6b
Rhony <5601175+rhonyabdullah@users.noreply.github.com>
2025-04-22 15:06:42 +0700

    18. Coin Presentation - ViewModel

---
+++ composeApp/src/commonMain/kotlin/com/learn/cmm/presentation/CoinListViewModel.kt
@@ -0,0 +1,53 @@
+package com.learn.cmm.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.learn.cmm.core.domain.Result
+import com.learn.cmm.domain.GetCoinListUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+
+class CoinListViewModel(
+    private val getCoinListUseCase: GetCoinListUseCase
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(CoinState())
+    val state = _state.onStart {
+        getCoinList()
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(),
+        initialValue = CoinState()
+    )
+
+    private suspend fun getCoinList() {
+        when (val response = getCoinListUseCase.execute()) {
+            is Result.Success -> _state.update {
+                CoinState(
+                    coins = response.data.map { item ->
+                        UiCoinListItem(
+                            id = item.coin.id,
+                            name = item.coin.name,
+                            iconUrl = item.coin.iconUrl,
+                            symbol = item.coin.symbol,
+                            formattedPrice = item.price.toString(), //TODO: formatFiat(coinItem.price),
+                            formattedChange = item.change.toString(), //TODO: formatPercentage(coinItem.change),
+                            isPositive = item.change >= 0,
+                        )
+                    }
+                )
+            }
+
+            is Result.Error<*> -> _state.update {
+                it.copy(
+                    coins = emptyList(),
+                    error = null //TODO: coinsResponse.error.toUiText()
+                )
+            }
+        }
+    }
+
+}
