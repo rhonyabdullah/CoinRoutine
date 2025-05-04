7362d7588282cea9cee57fe475877feea43cca8f
Rhony <5601175+rhonyabdullah@users.noreply.github.com>
2025-05-04 21:48:18 +0700

    34. Portofolio Repository Implement

--- composeApp/src/commonMain/kotlin/com/learn/cmm/portofolio/data/PortfolioRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/learn/cmm/portofolio/data/PortfolioRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.learn.cmm.portofolio.data
 
+import androidx.sqlite.SQLiteException
 import com.learn.cmm.core.domain.DataError
+import com.learn.cmm.core.domain.EmptyResult
 import com.learn.cmm.domain.api.CoinsRemoteDataSource
 import com.learn.cmm.portofolio.data.local.PortfolioDao
 import com.learn.cmm.portofolio.data.local.UserBalanceDao
@@ -14,8 +16,10 @@ import kotlinx.coroutines.flow.flow
 import com.learn.cmm.core.domain.Result
 import com.learn.cmm.core.domain.onError
 import com.learn.cmm.core.domain.onSuccess
+import com.learn.cmm.portofolio.data.mapper.toPortfolioCoinEntity
 import com.learn.cmm.portofolio.data.mapper.toPortfolioCoinModel
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.combine
 
 class PortfolioRepositoryImpl(
     private val portfolioDao: PortfolioDao,
@@ -24,18 +28,20 @@ class PortfolioRepositoryImpl(
 ) : PortfolioRepository {
 
     override suspend fun initializeBalance() {
-        val cashBalance = userBalanceDao.getCashBalance()
-        if (cashBalance == null) {
+        val currentBalance = userBalanceDao.getCashBalance()
+        if (currentBalance == null) {
             userBalanceDao.insertBalance(
-                UserBalanceEntity(cashBalance = 10000.0)
+                UserBalanceEntity(
+                    cashBalance = 10000.0
+                )
             )
         }
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     override fun allPortfolioCoinsFlow(): Flow<Result<List<PortfolioCoinModel>, DataError.Remote>> {
-        return portfolioDao.getAllOwnedCoins().flatMapLatest { portfolioCoinsEntities ->
-            if (portfolioCoinsEntities.isEmpty()) {
+        return portfolioDao.getAllOwnedCoins().flatMapLatest { portfolioCoinEntities ->
+            if (portfolioCoinEntities.isEmpty()) {
                 flow {
                     emit(Result.Success(emptyList<PortfolioCoinModel>()))
                 }
@@ -47,11 +53,11 @@ class PortfolioRepositoryImpl(
                         }
                         .onSuccess { coinsDto ->
                             val portfolioCoins =
-                                portfolioCoinsEntities.mapNotNull { portfolioCoinsEntity ->
+                                portfolioCoinEntities.mapNotNull { portfolioCoinEntity ->
                                     val coin =
-                                        coinsDto.data.coins.find { it.uuid == portfolioCoinsEntity.coinId }
+                                        coinsDto.data.coins.find { it.uuid == portfolioCoinEntity.coinId }
                                     coin?.let {
-                                        portfolioCoinsEntity.toPortfolioCoinModel(it.price)
+                                        portfolioCoinEntity.toPortfolioCoinModel(it.price)
                                     }
                                 }
                             emit(Result.Success(portfolioCoins))
@@ -61,5 +67,89 @@ class PortfolioRepositoryImpl(
         }.catch {
             emit(Result.Error(DataError.Remote.UNKNOWN))
         }
+    }
+
+    override suspend fun getPortfolioCoin(coinId: String): Result<PortfolioCoinModel?, DataError.Remote> {
+        coinsRemoteDataSource.getCoinById(coinId)
+            .onError { error ->
+                return Result.Error(error)
+            }
+            .onSuccess { coinDto ->
+                val portfolioCoinEntity = portfolioDao.getCoinById(coinId)
+                return if (portfolioCoinEntity != null) {
+                    Result.Success(portfolioCoinEntity.toPortfolioCoinModel(coinDto.data.coin.price))
+                } else {
+                    Result.Success(null)
+                }
+            }
+        return Result.Error(DataError.Remote.UNKNOWN)
+    }
+
+    override suspend fun savePortfolioCoin(portfolioCoin: PortfolioCoinModel): EmptyResult<DataError.Local> {
+        try {
+            portfolioDao.insert(portfolioCoin.toPortfolioCoinEntity())
+            return Result.Success(Unit)
+        } catch (_: SQLiteException) {
+            return Result.Error(DataError.Local.DISK_FULL)
+        }
+    }
+
+    override suspend fun removeCoinFromPortfolio(coinId: String) {
+        portfolioDao.deletePortfolioItem(coinId)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override fun calculateTotalPortfolioValue(): Flow<Result<Double, DataError.Remote>> {
+        return portfolioDao.getAllOwnedCoins().flatMapLatest { portfolioCoinsEntities ->
+            if (portfolioCoinsEntities.isEmpty()) {
+                flow {
+                    emit(Result.Success(0.0))
+                }
+            } else {
+                flow {
+                    val apiResult = coinsRemoteDataSource.getListOfCoins()
+                    apiResult.onError { error ->
+                        emit(Result.Error(error))
+                    }.onSuccess { coinsDto ->
+                        val totalValue = portfolioCoinsEntities.sumOf { ownedCoin ->
+                            val coinPrice =
+                                coinsDto.data.coins.find { it.uuid == ownedCoin.coinId }?.price
+                                    ?: 0.0
+                            ownedCoin.amountOwned * coinPrice
+                        }
+                        emit(Result.Success(totalValue))
+                    }
+                }
+            }
+        }.catch {
+            emit(Result.Error(DataError.Remote.UNKNOWN))
+        }
+    }
+
+    override fun cashBalanceFlow(): Flow<Double> {
+        return flow {
+            emit(userBalanceDao.getCashBalance() ?: 10000.0)
+        }
+    }
+
+    override fun totalBalanceFlow(): Flow<Result<Double, DataError.Remote>> {
+        return combine(
+            cashBalanceFlow(),
+            calculateTotalPortfolioValue()
+        ) { cashBalance, portfolioResult ->
+            when (portfolioResult) {
+                is Result.Success -> {
+                    Result.Success(cashBalance + portfolioResult.data)
+                }
+
+                is Result.Error -> {
+                    Result.Error(portfolioResult.error)
+                }
+            }
+        }
+    }
+
+    override suspend fun updateCashBalance(newBalance: Double) {
+        userBalanceDao.updateCashBalance(newBalance)
     }
 }
--- composeApp/src/commonMain/kotlin/com/learn/cmm/portofolio/domain/PortfolioRepository.kt
+++ composeApp/src/commonMain/kotlin/com/learn/cmm/portofolio/domain/PortfolioRepository.kt
@@ -1,6 +1,7 @@
 package com.learn.cmm.portofolio.domain
 
 import com.learn.cmm.core.domain.DataError
+import com.learn.cmm.core.domain.EmptyResult
 import com.learn.cmm.core.domain.Result
 import kotlinx.coroutines.flow.Flow
 
@@ -8,4 +9,13 @@ interface PortfolioRepository {
 
     suspend fun initializeBalance()
     fun allPortfolioCoinsFlow(): Flow<Result<List<PortfolioCoinModel>, DataError.Remote>>
+
+    suspend fun getPortfolioCoin(coinId: String): Result<PortfolioCoinModel?, DataError.Remote>
+    suspend fun savePortfolioCoin(portfolioCoin: PortfolioCoinModel): EmptyResult<DataError.Local>
+    suspend fun removeCoinFromPortfolio(coinId: String)
+
+    fun calculateTotalPortfolioValue(): Flow<Result<Double, DataError.Remote>>
+    fun totalBalanceFlow(): Flow<Result<Double, DataError.Remote>>
+    fun cashBalanceFlow(): Flow<Double>
+    suspend fun updateCashBalance(newBalance: Double)
 }
