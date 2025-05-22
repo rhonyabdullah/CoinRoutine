fe2843ff2871846218c731c8a8b16020f6f394a0
Rhony <5601175+rhonyabdullah@users.noreply.github.com>
2025-05-22 21:47:52 +0700

    61. Test(Portfolio): Add PortfolioViewModel tests
    This commit adds unit tests for the `PortfolioViewModel` to verify its state management and portfolio value calculations.
    
    The tests cover:
    - Ensuring the initial state has an empty list of coins.
    - Verifying that when a portfolio coin is saved, the state is updated with the new coin and loading is set to false.
    - Confirming that the portfolio value updates correctly when a coin is added.

--- composeApp/src/commonTest/kotlin/com/learn/cmm/portofolio/presentation/PortfolioViewModelTest.kt
+++ composeApp/src/commonTest/kotlin/com/learn/cmm/portofolio/presentation/PortfolioViewModelTest.kt
@@ -1,10 +1,17 @@
 package com.learn.cmm.portofolio.presentation
 
+import app.cash.turbine.test
 import com.learn.cmm.portofolio.data.FakePortfolioRepository
 import com.learn.cmm.portofolio.domain.PortfolioRepository
+import com.learn.cmm.utils.formatFiat
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
 import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class PortfolioViewModelTest {
@@ -20,4 +27,43 @@ class PortfolioViewModelTest {
             dispatcher = UnconfinedTestDispatcher()
         )
     }
+
+    @Test
+    fun `state and portfolio coins are properly combined`() = runTest {
+        viewModel.state.test {
+            val initialState = awaitItem()
+            assertTrue(initialState.coins.isEmpty())
+
+            val portFolioCoin = FakePortfolioRepository.portfolioCoin
+            portfolioRepository.savePortfolioCoin(portFolioCoin)
+
+            awaitItem()
+            val updatedState = awaitItem()
+            assertTrue(updatedState.coins.isNotEmpty())
+            assertFalse(updatedState.isLoading)
+            assertEquals(
+                FakePortfolioRepository.portfolioCoin.coin.id,
+                updatedState.coins.first().id
+            )
+        }
+    }
+
+    @Test
+    fun `portfolio value updates when a coin is added`() = runTest {
+        viewModel.state.test {
+            val initiateState = awaitItem()
+            assertEquals(initiateState.portfolioValue, formatFiat(10000.0))
+
+            val portfolioCoin = FakePortfolioRepository.portfolioCoin.copy(
+                ownedAmountInUnit = 50.0,
+                ownedAmountInFiat = 1000.0
+            )
+
+            portfolioRepository.savePortfolioCoin(portfolioCoin)
+            val updatedState = awaitItem()
+            assertEquals(formatFiat(11000.0), updatedState.portfolioValue)
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
 }
